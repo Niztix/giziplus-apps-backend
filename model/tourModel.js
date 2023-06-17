f09fb5c39d3dd3bce488c195a6f10634d51fd448
Muhammad Nur Ilmi <mnurilmi2016@gmail.com>
2023-06-17 20:57:52 +0700

    adding title model

--- model/tourModel.js
+++ model/tourModel.js
@@ -6,6 +6,11 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Field ini harus diisi!'],
     minLength: [4, 'Nama harus diisi minimal 4 karakter!']
   },
+  title: {
+    type: String,
+    required: [true, 'Field ini harus diisi!'],
+    minLength: [4, 'Title ini harus disini minimal 4 karakter']
+  },
   description: {
     type: String,
     required: [true, 'Field ini harus'],
